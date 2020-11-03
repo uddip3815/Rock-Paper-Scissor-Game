2d477303a0192264ae26c86b6fc0425bf422151d
uddip3815 <71618481+uddip3815@users.noreply.github.com>
2020-11-03 11:53:11 +0530

    Update StonePaper.py

--- StonePaper.py
+++ StonePaper.py
@@ -2,28 +2,28 @@ import random as rd
 a = rd.randint(1, 3)
 print("Computer's Turn: ")
 if a==1:
-    comp = "Stone"
+    comp = "Rock"
 elif a==2:
     comp = "Paper"
 elif a==3:
     comp = "Scissor"
 me = input("Your Turn: ")
 if me=="Stone":
-    if comp=="Stone":
+    if comp=="Rock":
         print("Tie!")
     elif comp=="Paper":
         print("You Lose!")
     elif comp=="Scissor":
         print("You Won!")
 elif me=="Paper":
-    if comp=="Stone":
+    if comp=="Rock":
         print("You Won!")
     elif comp=="Paper":
         print("Tie!")
     elif comp=="Scissor":
         print("You Lose!")
 elif me=="Scissor":
-    if comp=="Stone":
+    if comp=="Rock":
         print("You Lose!")
     elif comp=="Paper":
         print("You Won!")
@@ -31,4 +31,3 @@ elif me=="Scissor":
         print("Tie!")
 print(f"You Choose {me}")
 print(f"Computer Choose {comp}")
-print(input(""))
